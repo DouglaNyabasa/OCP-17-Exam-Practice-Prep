3b94f2db87edccfd454150dec24df3819195e632
douglanyabasa <douglasnyabasa400@gmail.com>
2025-07-15 16:19:15 +0200

    predicate update

---
+++ Chapter-8 -Lambdas and Functional Interfaces/Functional-Interfaces/Predicate/PredicateDemo.java
@@ -0,0 +1,18 @@
+import java.util.function.Predicate;
+
+public class PredicateDemo {
+    public static void main(String[] args) {
+        Predicate<Integer> predicate = t-> {
+            if(t % 2== 0){
+                return true;
+            }
+            else{
+                return false;
+            }
+        };
+        System.out.println(predicate.test(9));
+    }
+
+}
+
+
