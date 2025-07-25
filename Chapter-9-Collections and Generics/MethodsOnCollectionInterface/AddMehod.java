249aaf54ddb0292897985080ab2f48ac52c1a142
douglanyabasa <douglasnyabasa400@gmail.com>
2025-07-25 11:03:55 +0200

    add method

---
+++ Chapter-9-Collections and Generics/MethodsOnCollectionInterface/AddMehod.java
@@ -0,0 +1,14 @@
+import java.util.Collection;
+import java.util.ArrayList;
+
+public class AddingMethed{
+   
+   public static void main(String[] args){
+   Collection<String> list = new ArrayList<>();
+   list.add("Goku");
+   list.add("naruto");
+   System.out.println(list);
+
+   }
+
+}
