49306a0e63c3580b63161d2d127d093274665ce6
douglanyabasa <douglasnyabasa400@gmail.com>
2025-07-25 11:32:17 +0200

    removing method

---
+++ Chapter-9-Collections and Generics/MethodsOnCollectionInterface/RemovingMethod/RemovingMehod.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class RemovingMethod {
+
+    public static void main(String[] args) {
+        Collection<String> list = new ArrayList<>();
+        list.add("jujutsu kaisen");
+        list.add("bleach");
+        list.remove("bleach");
+        System.out.println(list);
+    }
+
+
+}
