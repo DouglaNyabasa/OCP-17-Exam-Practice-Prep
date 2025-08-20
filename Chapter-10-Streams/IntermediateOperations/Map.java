a4005f47ecb96ed92b7244612e8a34674415f031
douglanyabasa <douglasnyabasa400@gmail.com>
2025-08-20 11:17:39 +0200

    map intermediate operation

---
+++ Chapter-10-Streams/IntermediateOperations/Map.java
@@ -0,0 +1,8 @@
+import java.util.stream.Stream;
+
+public class Map{
+  public static void main(String [] args){
+  Stream<String> shows = Stream.of("superman vs batman", "mission impossible","doomsday avangers","thunder-bolts");
+  shows.map(x-> x.length()).forEach(y->System.out.println(y));
+  }
+}
