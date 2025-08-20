140fef07d7c64c48d0cbcd4a95d833bacff5539c
douglanyabasa <douglasnyabasa400@gmail.com>
2025-08-20 15:31:01 +0200

    flatmap intermediate operation

---
+++ Chapter-10-Streams/IntermediateOperations/FlatMap.java
@@ -0,0 +1,14 @@
+import java.util.List;
+import java.util.stream.Stream;
+
+
+public class FlatMap{
+ public static void main(String [] args){
+   List<String> fighters = List.of();
+   var group1 = List.of("Itachi","Jeraiya","Kakashe hatake");
+   var group2 = List.of("Obito","Madara Uchiha","Sakura");
+   var group3 = List.of("Naruto","Sasuke","Ourochimaru");
+   Stream<List<String>> chuninExamFights = Stream.of(group1,group2,group3);
+   chuninExamFights.flatMap(x -> x.stream()).forEach(System.out::println);
+ }
+}
