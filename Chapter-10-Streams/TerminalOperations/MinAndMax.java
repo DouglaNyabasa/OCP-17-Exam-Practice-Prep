557452d852fa1eab47bbe19b57f1f21b8b571a4a
douglanyabasa <douglasnyabasa400@gmail.com>
2025-08-22 15:25:21 +0200

    min max fix

--- Chapter-10-Streams/TerminalOperations/MinAndMax.java
+++ Chapter-10-Streams/TerminalOperations/MinAndMax.java
@@ -2,7 +2,7 @@ import java.util.stream.Stream;
 
 public class inAndMax{
     public static void main(String[] args) {
-        Stream<String> animeshows = Stream.of("dragon ball z", "attack on titan ","black clover ","JJK");
+        Stream<String> animeshows = Stream.of("one punch man","dragon ball z", "attack on titan ","black clover ","JJK");
        Stream<String> animeshows1 = Stream.of("dragon ball z", "attack on titan ","black clover ","JJK","demon slayer");
         System.out.println(animeshows.min((a,b)-> a.length()-b.length()));
         System.out.println(animeshows1.max((a,b)-> a.length()-b.length()));
